ab19bb3437219c7341fa3d2c7211038e92cbdd8b
Rodrigo Bersa <rodrigo.bersa@gmail.com>
2023-06-28 19:15:00 -0400

    Running tests

---
+++ .generate-providers.sh
@@ -0,0 +1,9 @@
+terraform-config-inspect --json . | jq -r '
+  [.required_providers[].aliases]
+  | flatten
+  | del(.[] | select(. == null))
+  | reduce .[] as $entry (
+    {};
+    .provider[$entry.name] //= [] | .provider[$entry.name] += [{"alias": $entry.alias}]
+  )
+' | tee aliased-providers.tf.json
